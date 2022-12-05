7144fc56d3b69675edb4a1b29e1cd4bf1630825c
Khushi Jaiswal <14khushijais@gmail.com>
2022-12-05 05:24:37 +0000

    get filds directly from model class

--- things/forms.py
+++ things/forms.py
@@ -2,13 +2,14 @@
 
 # Create your forms here.
 from django import forms
+from django.core.validators import MinValueValidator, MaxValueValidator
+from .models import Thing
 
-class ThingForm(forms.Form):
-    name = forms.CharField(label='Name', max_length=35)
-    description = forms.CharField(label='Description', max_length=120, widget=forms.Textarea)
-    quantity = forms.IntegerField(widget=forms.NumberInput)
-    # description = forms.Textarea(label='Description', max_length=120)
-    # quantity = forms.NumberInput(label='Quantity', max_length=50)
-
-
-    # min_length = 0 for quantity?
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = {'description': forms.Textarea(),'quantity': forms.NumberInput()}
+    #name = forms.CharField(label='Name', max_length=35)
+    #description = forms.CharField(label='Description', max_length=120, widget=forms.Textarea)
+    #quantity = forms.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(50)], widget=forms.NumberInput)
